c12155053e8b31c58e23dcd765d794a7f81a5078
Markin Petr <skyhome5041@icloud.com>
2022-08-09 23:28:19 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.average = Math.floor(((this.min + 1) + this.max) / 2);
+        console.log(this.average)
+        return this.average;
     }
 
     lower() {
-
+        this.max = this.average
     }
 
     greater() {
-
+        this.min = this.average
     }
 }
 
